63ddf897621e713631e708b4964a94755ee63a30
Steve Miskovetz <steve.miskovetz@gmail.com>
2023-06-24 16:04:12 -0600

    Directly assign memo, if empty, it'll be nil

--- x/wasm/keeper/handler_plugin_encoders.go
+++ x/wasm/keeper/handler_plugin_encoders.go
@@ -285,9 +285,7 @@ func EncodeIBCMsg(portSource types.ICS20TransferPortSource) func(ctx sdk.Context
 				Receiver:         msg.Transfer.ToAddress,
 				TimeoutHeight:    ConvertWasmIBCTimeoutHeightToCosmosHeight(msg.Transfer.Timeout.Block),
 				TimeoutTimestamp: msg.Transfer.Timeout.Timestamp,
-			}
-			if msg.Transfer.Memo != nil {
-				msg.Memo = msg.Transfer.Memo
+				Memo: 			  msg.Transfer.Memo,
 			}
 			return []sdk.Msg{msg}, nil
 		default:
